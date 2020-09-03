7f2d2150a50a9f300a67ccd97b2aaa0fd715c54e
Gaurav <gauravsingh902438@gmail.com>
2020-09-03 10:28:33 +0530

    car detector

---
+++ Car Detection/car detector.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 15 17:32:35 2020
+
+@author: gaurav
+"""
+
+import cv2 
+
+CarCascade = cv2.CascadeClassifier("cars.xml")
+
+def detect_cars(frame):
+    cars=CarCascade.detectMultiScale(frame,1.1 , 1)
+    for (x,y,w,h) in cars:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+    return frame
+carvideo=cv2.VideoCapture('Cars.mp4')
+def simulator():
+    
+    while carvideo.isOpened():
+        ret, frame=carvideo.read()
+        controlkey=cv2.waitKey(1)
+        if ret:
+            cars_frame=detect_cars(frame)
+            cv2.imshow('frame',cars_frame)
+        else:
+            break
+        if controlkey==ord('q'):
+            break
+
+if __name__ == '__main__':
+    simulator()
+carvideo.release()
+cv2.destroyAllWindows()
+    
